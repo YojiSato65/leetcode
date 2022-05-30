98bcd14afe66c8cd61428d5ed6c21bcc3df2b5f9
YojiSato65 <yojisato65@gmail.com>
2022-05-30 16:51:12 +0300

    Solved twoSum

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Two Sum
+
+const twoSum = function (nums, target)
+{
+    for (i = 0; i < nums.length; i++)
+    {
+        for (j = 0; j < nums.length; j++)
+        {
+            if (i !== j)
+            {
+                if (nums[i] + nums[j] === target)
+                {
+                    return [i, j]
+                }
+            }
+        }
+    }
+}
+console.log(twoSum([2, 7, 11, 15], 9))
