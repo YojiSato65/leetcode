1585daa9dfaec0934c76dce7826018859f74c49a
YojiSato65 <yojisato65@gmail.com>
2022-05-31 22:56:15 +0300

    solved Palindrome Number

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 // 1. Two Sum
 
-const twoSum = function (nums, target)
-{
-    for (i = 0; i < nums.length; i++)
-    {
-        for (j = 0; j < nums.length; j++)
-        {
-            if (i !== j)
-            {
-                if (nums[i] + nums[j] === target)
-                {
-                    return [i, j]
-                }
-            }
-        }
-    }
-}
-console.log(twoSum([2, 7, 11, 15], 9))
+// const twoSum = function (nums, target)
+// {
+//     for (i = 0; i < nums.length; i++)
+//     {
+//         for (j = i + 1; j < nums.length; j++)
+//         {
+//             if (nums[i] + nums[j] === target)
+//             {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+// console.log(twoSum([2, 3, 4], 7))
+
+
+// 2. Palindrome Number
+
+// const isPalindrome = function (x)
+// {
+//     let rev_num = 0
+//     let num = x
+//     console.log('rev_num', rev_num, 'x', x)
+//     while (num > 0)
+//     {
+//         rev_num = rev_num * 10 + num % 10
+//         num = Math.floor(num / 10)
+//     }
+
+//     if (x === rev_num)
+//     {
+//         return true
+//     } return false
+// }
+// console.log(isPalindrome(13221))
